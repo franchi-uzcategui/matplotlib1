89d747af858940d08e0462bcaad584b027a9fd26
Franchi <32370428+franchica@users.noreply.github.com>
2018-04-05 15:08:09 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,12 @@
+# Creating a line graph from data
+#Changing the appearance of the line
+#Zooming in on different parts of the axis
+#Putting labels on titles and axes
+#Creating a more complex figure layout
+#Adding legends to graphs
+#Changing tick labels and positions
+#Saving what you've made'''
+
 import codecademylib
 from matplotlib import pyplot as plt
 x=[0,1,2,3,4,5]
